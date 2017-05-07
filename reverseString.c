483cd814ea76a921492f8e5a3530117786a73904
ss83032 <ss83032@gmail.com>
2017-05-07 22:20:22 +0800

    Update reverseString.c

--- reverseString.c
+++ reverseString.c
@@ -3,11 +3,18 @@
 #include <string.h>
 #include <stdbool.h>
 
+#ifdef __WIN32__ // or whatever
+#define SSIZET_FMT "%lu"
+#else
+#define SSIZET_FMT "%zu"
+#endif
+
 bool reverseString_Basic(char* pcInput, char* pcOutput);
 
 int main(int argc, char *argv[]) {
-  char InputString[] = "123456789";
-  char *OutputString = malloc(0);
+	char InputString[] = "1 2 3 4 5 6 7 8 9";
+	// char *OutputString = malloc(0);
+	char OutputString[100];
 	reverseString_Basic(InputString, OutputString);
 	printf("OutputString=%s\n", OutputString);
 	return 0;
@@ -15,9 +22,9 @@ int main(int argc, char *argv[]) {
 
 bool reverseString_Basic(char* pcInput, char* pcOutput) {
 	printf("-----reverseString_Basic-----\n");
-	int len = strlen(pcInput); 
-	printf("InputString=%s, Len=%d\n", pcInput, len);
-	pcOutput = realloc(pcOutput, len);
+	const size_t len = strlen(pcInput); 
+	printf("InputString=%s, Len="SSIZET_FMT"\n", pcInput, len);
+	// pcOutput = malloc(pcOutput, len);
 	char* p = pcOutput; 
 	int i;
 	for (i = len - 1 ; i >= 0 ; i--) {
